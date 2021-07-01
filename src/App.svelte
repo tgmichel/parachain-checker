d5f4b6381a10c0e58a3169ad1af92fadda144c22
tgmichel <telmo@purestake.com>
2021-07-01 18:38:32 +0200

    Style and wrap-up

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,76 @@
 <script>
-	export let name;
+	export let config;
+	import Relay from './Relay.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each config.relays as data}
+		<Relay data = {data}/>
+	{/each}
 </main>
 
 <style>
+	:global(body) {
+		background-color: #edeaf6;
+		font-family: 'Titillium Web', sans-serif;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+		position: relative;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
+	@media (min-width: 1024px) {
 		main {
-			max-width: none;
+			max-width: 50%;
 		}
+	}
+
+	:global(table.providers) {
+		border: 1px solid #FFF;
+		background-color: #EEEEEE;
+		width: 100%;
+		text-align: left;
+		border-collapse: collapse;
+	}
+
+	:global(table.providers td) {
+		border: 1px solid #FFF;
+		padding: 3px 2px;
+	}
+
+	:global(table.providers th) {
+		border: 1px solid #FFF;
+		padding: 3px 2px;
+	}
+
+	:global(table.providers tbody td) {
+		
+        font-size: 1.2em;
+	}
+
+	:global(table.providers tr:nth-child(even)) {
+		background: #e1ddee;
+	}
+
+	:global(table.providers thead) {
+		background: #1C6EA4;
+		background: -moz-linear-gradient(top, #9855bb 0%, #8932ad 66%, #761ca4 100%);
+		background: -webkit-linear-gradient(top, #9855bb 0%, #8932ad 66%, #761ca4 100%);
+		background: linear-gradient(to bottom, #9855bb 0%, #8932ad 66%, #761ca4 100%);
+		border-bottom: 2px solid #FFF;
+	}
+
+	:global(table.providers thead th) {
+		font-size: 15px;
+		font-weight: bold;
+		color: #FFFFFF;
+		border-left: 2px solid #FFF;
+	}
+
+	:global(table.providers thead th:first-child) {
+		border-left: none;
 	}
 </style>
